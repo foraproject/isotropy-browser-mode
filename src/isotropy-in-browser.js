27416eb3e8dbd341fb0d6593018efa8e6c3d31c3
Jeswin Kumar <jeswinpk@agilehead.com>
2016-03-05 10:13:57 +0530

    WIP

--- src/isotropy-in-browser.js
+++ src/isotropy-in-browser.js
@@ -4,11 +4,13 @@ import urlMiddleware from "isotropy-middleware-url";
 import bodyMiddleware from "isotropy-middleware-body";
 import Router from "isotropy-router";
 import type { IsotropyOptionsType, IsotropyResultType } from "isotropy-core";
-import type { IncomingMessage, ServerResponse, Server } from "isotropy-interfaces/node/http-in-browser";
+import type { IncomingMessage, ServerResponse, Server } from "isotropy-interfaces/node/http";
 
 type IsotropyFnType = (apps: Object, options: IsotropyOptionsType) => Promise<IsotropyResultType>;
 
 export default async function(apps: Object, plugins: any, options: IsotropyOptionsType) : Promise<IsotropyResultType> {
+  const isotropy: IsotropyFnType = getIsotropy(plugins);
+
   options.handler = (router: Router) => (req: IncomingMessage, res: ServerResponse) => {
     urlMiddleware(req, res)
     .then(() => bodyMiddleware(req, res))
